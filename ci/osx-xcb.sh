6834481759ffc0fd9347fe6caa4a7eff8f4b977c
Chris Frank <cfrank@redventures.com>
2019-02-07 12:21:02 -0500

    revert

--- ci/osx-xcb.sh
+++ ci/osx-xcb.sh
@@ -26,7 +26,7 @@ git_clone() {
     autoreconf -fvi
     ACLOCAL="aclocal -I /opt/X11/share/aclocal -I /usr/local/share/aclocal"\
     PKG_CONFIG_PATH="/opt/X11/share/pkgconfig:/opt/X11/lib/pkgconfig:${PKG_CONFIG_PATH}"\
-    ./configure --prefix=/opt/X11 --disable-dependency-tracking
+    ./autogen.sh --prefix=/opt/X11 --disable-dependency-tracking
 
     make
 
