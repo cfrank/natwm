8446ca7d45a8377a8ac4c1d6f25027bb006e4754
Chris Frank <chris@cfrank.org>
2019-02-06 19:56:47 -0500

    Fix sh syntax error
    Signed-off-by: Chris Frank <chris@cfrank.org>

--- ci/install.sh
+++ ci/install.sh
@@ -2,6 +2,7 @@
 
 if [[ $TRAVIS_OS_NAME == "linux" ]]; then
     # Linux deps
+    echo "Any linux deps would be installed now..."
 elif [[ $TRAVIS_OS_NAME == "osx" ]]; then
     # OSX deps
     cd ci
