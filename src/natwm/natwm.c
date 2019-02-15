0f1f28b6f18c45264a9f7ff3d78484724a55f1a0
Chris Frank <cfrank@redventures.com>
2019-02-15 14:40:51 -0500

    Handle errors from x

--- src/natwm/natwm.c
+++ src/natwm/natwm.c
@@ -6,16 +6,32 @@
 
 static void handle_connection_error(xcb_connection_t *connection, int error)
 {
-        printf("%s", "There was an error connecting to the x server");
+        const char *message;
+
+        switch (error) {
+        case XCB_CONN_CLOSED_EXT_NOTSUPPORTED:
+                message = "Connection to the X Server failed: Extension not "
+                          "supported";
+        case XCB_CONN_CLOSED_MEM_INSUFFICIENT:
+                message = "Connection to the X server failed: Lack of memory";
+        case XCB_CONN_CLOSED_REQ_LEN_EXCEED:
+                message = "Connection to the X server failed: Invalid request";
+        case XCB_CONN_CLOSED_PARSE_ERR:
+                message = "Connection to the X server failed: Invalid display";
+        case XCB_CONN_CLOSED_INVALID_SCREEN:
+                message = "Connection to the X server failed: Screen not found";
+        default:
+                message = "Connection to the X server failed";
+        }
+
+        LOG_CRITICAL_SHORT(natwm_logger, message);
 
         xcb_disconnect(connection);
 }
 
-static xcb_connection_t *make_connection(void)
+static xcb_connection_t *make_connection(int *screen_num)
 {
-        int screenp;
-
-        xcb_connection_t *connection = xcb_connect(NULL, &screenp);
+        xcb_connection_t *connection = xcb_connect(NULL, screen_num);
 
         int connection_error = xcb_connection_has_error(connection);
 
@@ -30,10 +46,12 @@ static xcb_connection_t *make_connection(void)
 
 int main(void)
 {
+        int screen_num;
+
         // Initialize the logger
         initialize_logger();
 
-        xcb_connection_t *connection = make_connection();
+        xcb_connection_t *connection = make_connection(&screen_num);
 
         if (connection == NULL) {
                 exit(EXIT_FAILURE);
