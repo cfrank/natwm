fe1f75cc632d880078a0f2bee38844c847c5ae4e
Chris Frank <chris@cfrank.org>
2019-02-16 12:02:30 -0500

    Fix bug with fall through on xcb_conn error
    Signed-off-by: Chris Frank <chris@cfrank.org>

--- src/natwm/natwm.c
+++ src/natwm/natwm.c
@@ -12,14 +12,19 @@ static void handle_connection_error(xcb_connection_t *connection, int error)
         case XCB_CONN_CLOSED_EXT_NOTSUPPORTED:
                 message = "Connection to the X Server failed: Extension not "
                           "supported";
+                break;
         case XCB_CONN_CLOSED_MEM_INSUFFICIENT:
                 message = "Connection to the X server failed: Lack of memory";
+                break;
         case XCB_CONN_CLOSED_REQ_LEN_EXCEED:
                 message = "Connection to the X server failed: Invalid request";
+                break;
         case XCB_CONN_CLOSED_PARSE_ERR:
                 message = "Connection to the X server failed: Invalid display";
+                break;
         case XCB_CONN_CLOSED_INVALID_SCREEN:
                 message = "Connection to the X server failed: Screen not found";
+                break;
         default:
                 message = "Connection to the X server failed";
         }
