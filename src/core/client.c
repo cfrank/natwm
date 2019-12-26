98297621ef31cf7598d358a7ce8d4f00adda6fdb
Chris Frank <chris@cfrank.org>
2019-12-26 03:12:54 -0800

    core/client: initial shell of a client
    Signed-off-by: Chris Frank <chris@cfrank.org>

---
+++ src/core/client.c
@@ -0,0 +1,5 @@
+// Copyright 2019 Chris Frank
+// Licensed under BSD-3-Clause
+// Refer to the license.txt file included in the root of the project
+
+#include "client.h"
---
+++ src/core/client.h
@@ -0,0 +1,42 @@
+// Copyright 2019 Chris Frank
+// Licensed under BSD-3-Clause
+// Refer to the license.txt file included in the root of the project
+
+#pragma once
+
+enum client_type {
+        CLIENT_TYPE_DOCK,
+        CLIENT_TYPE_TOOLBAR,
+        CLIENT_TYPE_SPLASH,
+        CLIENT_TYPE_NORMAL,
+};
+
+enum client_status {
+        CLIENT_STATUS_NORMAL,
+        CLIENT_STATUS_URGENT,
+        CLIENT_STATUS_FOCUSED,
+        CLIENT_STATUS_STICKY,
+};
+
+/**
+ * A client is the frame which surrounds a mapped window. It can also be a
+ * empty frame which hasn't been populated with a window. In the case of a
+ * empty frame the rect will be the tiled size and the floating rect will
+ * be 0
+ *
+ * When a window is mapped to the screen two sizes will be saved to the client:
+ *
+ * 1) It's initial requested size will be saved as it's floating rect.
+ * 2) It's tile position will be stored as it's rect.
+ */
+struct client {
+        xcb_rectangle_t floating_rect;
+        xcb_rectangle_t rect;
+        enum client_type type;
+        enum client_status status;
+        bool is_floating;
+        xcb_window_t window;
+};
+
+struct client *client_create(xcb_window_t window);
+void client_destroy(struct client *client);
