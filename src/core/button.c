91180f647a168d158d452f76cef01479bd6e6ecc
Chris Frank <chris@cfrank.org>
2020-06-30 00:39:38 -0700

    core/button: Add button state
    This will allow us to both track the grabbed client and also initiaize
    keyboard modifiers to help fix a bug where we can't focus on a client
    when caps lock is active
    
    Signed-off-by: Chris Frank <chris@cfrank.org>

--- src/core/button.c
+++ src/core/button.c
@@ -2,9 +2,149 @@
 // Licensed under BSD-3-Clause
 // Refer to the license.txt file included in the root of the project
 
+#include <assert.h>
+#include <stdlib.h>
 #include <xcb/xcb.h>
+#include <xcb/xcb_keysyms.h>
+
+#include <common/logger.h>
 
 #include "button.h"
+
+// Heavily influenced from bspwm:
+// https://github.com/baskerville/bspwm/blob/master/src/pointer.c
+//
+// Given a xcb_keysym_t attempt to find and return the mod mask
+static uint16_t mask_from_keysym(xcb_key_symbols_t *symbols, xcb_keycode_t *modifiers,
+                                 uint16_t modifier_length, uint8_t keycodes_per_modifier,
+                                 xcb_keysym_t keysym)
+{
+        uint16_t result = 0;
+        xcb_keycode_t *keycodes = xcb_key_symbols_get_keycode(symbols, keysym);
+
+        if (keycodes == NULL) {
+                return result;
+        }
+
+        for (uint16_t i = 0; i < modifier_length; ++i) {
+                for (uint8_t j = 0; j < keycodes_per_modifier; ++j) {
+                        xcb_keycode_t modifier = modifiers[i * keycodes_per_modifier + j];
+
+                        if (modifier == XCB_NO_SYMBOL) {
+                                continue;
+                        }
+
+                        for (xcb_keycode_t *key = keycodes; *key != XCB_NO_SYMBOL; key++) {
+                                if (*key == modifier) {
+                                        result |= (uint16_t)(1 << i);
+                                }
+                        }
+                }
+        }
+
+        free(keycodes);
+
+        return result;
+}
+
+// Heavily influenced from bspwm:
+// https://github.com/baskerville/bspwm/blob/master/src/pointer.c
+//
+// We need to find the modifier mask for our modifier keys. This will allow us to correctly set
+// up button handlers in the future.
+//
+// An example is when you are trying to focus on a window with caps lock active. This no longer
+// resolves as just a simple XCB_BUTTON_INDEX_1 event it is a XCB_BUTTON_INDEX_1 event with
+// the additional CAPS_LOCK modifier active.
+static struct button_modifiers *resolve_button_modifiers(xcb_connection_t *connection)
+{
+        struct button_modifiers *modifiers = malloc(sizeof(struct button_modifiers));
+
+        if (modifiers == NULL) {
+                return NULL;
+        }
+
+        xcb_key_symbols_t *symbols = xcb_key_symbols_alloc(connection);
+
+        if (symbols == NULL) {
+                free(modifiers);
+
+                return NULL;
+        }
+
+        xcb_get_modifier_mapping_cookie_t cookie = xcb_get_modifier_mapping(connection);
+        xcb_get_modifier_mapping_reply_t *reply
+                = xcb_get_modifier_mapping_reply(connection, cookie, NULL);
+
+        if (reply == NULL || reply->keycodes_per_modifier < 1) {
+                free(modifiers);
+
+                xcb_key_symbols_free(symbols);
+
+                return NULL;
+        }
+
+        xcb_keycode_t *modifier_keycodes = xcb_get_modifier_mapping_keycodes(reply);
+
+        if (modifier_keycodes == NULL) {
+                free(modifiers);
+
+                xcb_key_symbols_free(symbols);
+
+                free(reply);
+
+                return NULL;
+        }
+
+        int modifier_length
+                = xcb_get_modifier_mapping_keycodes_length(reply) / reply->keycodes_per_modifier;
+
+        assert(modifier_length >= 0 && modifier_length <= UINT16_MAX);
+
+        modifiers->num_lock = mask_from_keysym(symbols,
+                                               modifier_keycodes,
+                                               (uint16_t)modifier_length,
+                                               reply->keycodes_per_modifier,
+                                               NUM_LOCK_KEYSYM);
+        modifiers->caps_lock = mask_from_keysym(symbols,
+                                                modifier_keycodes,
+                                                (uint16_t)modifier_length,
+                                                reply->keycodes_per_modifier,
+                                                CAPS_LOCK_KEYSYM);
+        modifiers->scroll_lock = mask_from_keysym(symbols,
+                                                  modifier_keycodes,
+                                                  (uint16_t)modifier_length,
+                                                  reply->keycodes_per_modifier,
+                                                  SCROLL_LOCK_KEYSYM);
+
+        if (modifiers->caps_lock == XCB_NO_SYMBOL) {
+                modifiers->caps_lock = XCB_MOD_MASK_LOCK;
+        }
+
+        xcb_key_symbols_free(symbols);
+        free(reply);
+
+        return modifiers;
+}
+
+struct button_state *button_state_create(xcb_connection_t *connection)
+{
+        struct button_state *state = malloc(sizeof(struct button_state));
+
+        if (state == NULL) {
+                return NULL;
+        }
+
+        state->modifiers = resolve_button_modifiers(connection);
+
+        if (state->modifiers == NULL) {
+                LOG_WARNING(natwm_logger, "Failed to resolve modifier keys! This may cause issues");
+        }
+
+        state->grabbed_client = NULL;
+
+        return state;
+}
 
 void button_event_grab(xcb_connection_t *connection, xcb_window_t window,
                        const struct button_binding *binding)
@@ -53,4 +193,13 @@ enum natwm_error button_handle_focus(struct natwm_state *state, struct workspace
         xcb_allow_events(state->xcb, XCB_ALLOW_REPLAY_POINTER, XCB_CURRENT_TIME);
 
         return NO_ERROR;
+}
+
+void button_state_destroy(struct button_state *state)
+{
+        if (state->modifiers != NULL) {
+                free(state->modifiers);
+        }
+
+        free(state);
 }
--- src/core/button.h
+++ src/core/button.h
@@ -17,6 +17,12 @@
 
 #define DEFAULT_BUTTON_MASK XCB_EVENT_MASK_BUTTON_PRESS | XCB_EVENT_MASK_BUTTON_RELEASE
 
+// Toggleable keys
+// https://cgit.freedesktop.org/xorg/proto/x11proto/tree/keysymdef.h
+#define NUM_LOCK_KEYSYM 0xff7f
+#define CAPS_LOCK_KEYSYM 0xffe5
+#define SCROLL_LOCK_KEYSYM 0xff14
+
 struct button_binding {
         uint8_t pass_event;
         uint16_t mask;
@@ -25,6 +31,17 @@ struct button_binding {
         xcb_cursor_t cursor;
         uint8_t button;
         uint16_t modifiers;
+};
+
+struct button_modifiers {
+        uint16_t num_lock;
+        uint16_t caps_lock;
+        uint16_t scroll_lock;
+};
+
+struct button_state {
+        struct button_modifiers *modifiers;
+        struct client *grabbed_client;
 };
 
 static const struct button_binding client_focus_event = {
@@ -49,6 +66,7 @@ static const struct button_binding button_events[BUTTON_EVENTS_NUM] = {
         },
 };
 
+struct button_state *button_state_create(xcb_connection_t *connection);
 void button_event_grab(xcb_connection_t *connection, xcb_window_t window,
                        const struct button_binding *binding);
 
@@ -57,3 +75,5 @@ void button_initialize_client_listeners(const struct natwm_state *state,
 
 enum natwm_error button_handle_focus(struct natwm_state *state, struct workspace *workspace,
                                      struct client *client);
+
+void button_state_destroy(struct button_state *state);
